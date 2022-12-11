fcb4af55224a60d07fd6fc2b595383dfcc954ae6
Pasu <89201116+Ciel289@users.noreply.github.com>
2022-12-11 13:24:20 +0700

    Update nodeBase.ino

--- nodeBase.ino
+++ nodeBase.ino
@@ -1,5 +1,5 @@
 //  Page 1
-// RX 
+// RX
 #include <SimpleTimer.h>
 SimpleTimer timer;
 
@@ -19,23 +19,22 @@ uint16_t allNode[4] = {node01, node02, node03, node04}; // Start at number 0 -->
 
 //  Line and Wifi
 #include <TridentTD_LineNotify.h>
-#define SSID        "-"      // Wifi's name 
-#define PASSWORD    "-"     // Wifi's password
-#define LINE_TOKEN  "-"   // รหัส TOKEN ที่ได้มาจากข้างบน
+#define SSID        "vivo 1902"      // Wifi's name  // vivo 1902  // OPPO A95
+#define PASSWORD    "gg123456"     // Wifi's password
+#define LINE_TOKEN  "lSMmKS2JyJHKDHeauWNonMprq6Fwj6xmm42epuahaI2"   // รหัส TOKEN ที่ได้มาจากข้างบน
 
 //  Function
-void _runT();
-void _chSt(byte inc);
 void _ttime();
+void _chSt(byte inc);
+void _runT();
 
 //  Struct for data
 struct dataRX {
   byte room = NULL;
   bool alert = false;
+  bool stat = false;
 };
 struct dataRX dataRX;
-
-bool roomSt = false;
 
 
 //  Start!
@@ -66,8 +65,8 @@ void setup() {
   radio.setDataRate(RF24_2MBPS);
   radio.printDetails();
 
-    timer.setInterval(1, _runT);
-    timer.setInterval(30L * 1000L, _ttime);
+  timer.setInterval(1, _runT);
+  timer.setInterval(30L * 1000L, _ttime);
 }
 
 void loop() {
@@ -77,9 +76,12 @@ void loop() {
 
 //---------------------------------------
 //  Page 2
-void _ttime() {
+
+
+void _ttime() { // Function for test
   dataRX.room = NULL;
   dataRX.alert = false;
+  dataRX.stat = false;
   _chSt(1); // Test send to node01
 }
 
@@ -89,14 +91,15 @@ void _chSt(byte inc) {
   network.update();
   RF24NetworkHeader header00(allNode[inc]); // (Address where the data is going)
   bool ok = network.write(header00, &stas, sizeof(stas));
-  roomSt = stas;
 }
 
 
 void _runT() {
   dataRX.room = NULL;
   dataRX.alert = false;
+  dataRX.stat = false;
 
+  network.update();
   while ( network.available() ) {
     RF24NetworkHeader header00;
     network.read(header00, &dataRX, sizeof(dataRX));
@@ -105,10 +108,9 @@ void _runT() {
   if (dataRX.alert) {
     for (byte i = 1; i <= 4; i++) {
       if (dataRX.room == i) {
-        if (roomSt) {
+        if (dataRX.stat) {
           Serial.println("No problem at Room" + String(i) + "!\t");
           LINE.notify("No problem at Room" + String(i) + "!");
-          roomSt = false;
         }
         else {
           Serial.println("Detected at Room" + String(i) + "! \t");
